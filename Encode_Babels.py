a32060dae60861c07376db44bb33e657026d8120
Scilence2022 <90053750+Scilence2022@users.noreply.github.com>
2024-08-17 07:38:09 +0800

    Update Encode_Babels.py

--- Encode_Babels.py
+++ Encode_Babels.py
@@ -7,6 +7,7 @@ from glass import Glass
 from crc16pure import *
 from test_utils import *
 import copy
+import os
 
 
 data_block_length = 30
@@ -16,31 +17,31 @@ fountain_seed = 2
 work_dir = r'./input_files'  + '/'
 
 # fileA = open(work_dir + r'Babel-A.jpg', 'rb')
-fileA = open(work_dir + r'tju.jpg', 'rb')
-fileB = open(work_dir + r'BCA.jpg', 'rb')
-fileC = open(work_dir + r'HECHAIN.jpg', 'rb')
-fileD = open(work_dir + r'ZONFF.jpg', 'rb')
+file_tju = open(work_dir + r'tju.jpg', 'rb')
+fileA = open(work_dir + r'BCA.jpg', 'rb')
+fileB = open(work_dir + r'HECHAIN.jpg', 'rb')
+fileC = open(work_dir + r'ZONFF.jpg', 'rb')
 
+filebytes_tju = file_tju.read()
+file_tju.close()
 filebytesA = fileA.read()
-fileA.close()
+file_tju.close()
 filebytesB = fileB.read()
-fileA.close()
+fileB.close()
 filebytesC = fileC.read()
 fileC.close()
-filebytesD = fileD.read()
-fileD.close()
 
-pass_a = bytes(8)
-pass_b = 0b10101110001111011000010100110110
-pass_c = 0b10011101011111101010101101110101
-pass_d = 0b00110011010000110010111001000011
+pass_default = bytes(8)
+pass_a = 0b10101110001111011000010100110110
+pass_b = 0b10011101011111101010101101110101
+pass_c = 0b00110011010000110010111001000011
 
 
 fountain_init_index = 3111111
-ft_a = DNAFountain(filebytesA, data_block_length, fountain_init_index, fountain_seed, 16, 16, 8)
-ft_a.fix_bytes()
-ft_a.des = True
-ft_a.sec_key = pass_a
+ft_default = DNAFountain(filebytes_tju, data_block_length, fountain_init_index, fountain_seed, 16, 16, 8)
+ft_default.fix_bytes()
+ft_default.des = True
+ft_default.sec_key = pass_default
 
 # for iiii in range(1, 6000):
 #     a = ft_a.DNAdroplet()
@@ -55,6 +56,10 @@ ft_a.sec_key = pass_a
 # len(a_dna)
 
 
+ft_a = DNAFountain(filebytesA, data_block_length, fountain_init_index, fountain_seed, 16, 16, 8)
+ft_a.des = True
+ft_a.sec_key = pass_a.to_bytes(8, byteorder ='big')
+
 ft_b = DNAFountain(filebytesB, data_block_length, fountain_init_index, fountain_seed, 16, 16, 8)
 ft_b.des = True
 ft_b.sec_key = pass_b.to_bytes(8, byteorder ='big')
@@ -63,10 +68,6 @@ ft_c = DNAFountain(filebytesC, data_block_length, fountain_init_index, fountain_
 ft_c.des = True
 ft_c.sec_key = pass_c.to_bytes(8, byteorder ='big')
 
-ft_d = DNAFountain(filebytesD, data_block_length, fountain_init_index, fountain_seed, 16, 16, 8)
-ft_d.des = True
-ft_d.sec_key = pass_d.to_bytes(8, byteorder ='big')
-
 failed_gts = []
 
 total_size = 3000 #int(fdna1.num_of_chunks * 1.15)
@@ -74,7 +75,7 @@ core_size = int(total_size*0.96)
 
 droplet_four_pics = []
 
-droplet_all = get_droplets(total_size, ft_a)
+droplet_all = get_droplets(total_size, ft_default)
 droplet_four_pics.append(droplet_all)
 suc_num_a = 0
 for i in range(0, test_num):
@@ -89,7 +90,7 @@ for i in range(0, test_num):
     #     suc_num_a = suc_num_a + 1
 
 
-droplet_all = get_droplets(total_size, ft_b)
+droplet_all = get_droplets(total_size, ft_a)
 droplet_four_pics.append(droplet_all)
 suc_num_b = 0
 for i in range(0, test_num):
@@ -103,7 +104,7 @@ for i in range(0, test_num):
     # if test_droplets(droplet_sample, ft_b):
     #     suc_num_b = suc_num_b + 1
 
-droplet_all = get_droplets(total_size, ft_c)
+droplet_all = get_droplets(total_size, ft_b)
 droplet_four_pics.append(droplet_all)
 suc_num_c = 0
 for i in range(0, test_num):
@@ -117,7 +118,7 @@ for i in range(0, test_num):
     # if test_droplets(droplet_sample, ft_c):
     #     suc_num_c = suc_num_c + 1
 
-droplet_all = get_droplets(total_size, ft_d)
+droplet_all = get_droplets(total_size, ft_c)
 droplet_four_pics.append(droplet_all)
 suc_num_d = 0
 for i in range(0, test_num):
@@ -169,6 +170,21 @@ for i in range(0, 4):
         file2.write(str(dps.tail_index))
         file2.write('\n')
     file2.close()
+
+
+os.system('mv input_files/babel_v2.DNAs.tab.sim.0 input_files/tju.jpg.strands')
+os.system('mv input_files/babel_v2.DNAs.tab.rich.0 input_files/tju.jpg.details')
+
+os.system('mv input_files/babel_v2.DNAs.tab.sim.1 input_files/BCA.jpg.strands')
+os.system('mv input_files/babel_v2.DNAs.tab.rich.1 input_files/BCA.jpg.details')
+
+os.system('mv input_files/babel_v2.DNAs.tab.sim.2 input_files/HECHAIN.jpg.strands')
+os.system('mv input_files/babel_v2.DNAs.tab.rich.2 input_files/HECHAIN.jpg.details')
+
+os.system('mv input_files/babel_v2.DNAs.tab.sim.3 input_files/ZONFF.jpg.strands')
+os.system('mv input_files/babel_v2.DNAs.tab.rich.3 input_files/ZONFF.jpg.details')
+
+
 
 
 
